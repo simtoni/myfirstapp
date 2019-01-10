9a6f8588d0b766dfa92cfcff4a924c95bb3785d9
Antonina Simakhova <antonina.simakhova@gmail.com>
2019-01-10 23:44:30 +0100

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'simple_pages#index'
 
   # landing page - featured product
-  root 'simple_pages#landing_page'
+  # root 'simple_pages#landing_page'
+
+  ### TASK 5.1
+  root 'products#index'
 
   # orders
   resources :orders, only: [:index, :show, :create, :destroy]
