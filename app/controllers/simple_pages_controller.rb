203f71e50e89aa9a8f3804a0f2446e390abfcb4d
Antonina Simakhova <antonina.simakhova@gmail.com>
2019-01-11 17:10:42 +0100

    5.3 Sending E-Mails

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,3 +8,13 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(3)
   end
 end
+
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
+
+  post 'simple_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # landing page - featured product
