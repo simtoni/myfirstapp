e8c9d37961507209b841678f50b361682abb94cc
Antonina Simakhova <antonina.simakhova@gmail.com>
2019-01-12 00:52:33 +0100

    5.2 + 5.3 corrections

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
+
   def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-end
+    if Rails.env.production? 
+      Product.where("name ilike ?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
+    end
+  end
+
 end
