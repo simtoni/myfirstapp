be12b491ae265b64956e9d9ee5866d09f9993e36
Antonina Simakhova <antonina.simakhova@gmail.com>
2019-01-14 21:50:24 +0100

    5.7 The Comment Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,20 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
-    if Rails.env.production? 
+    if Rails.env.production?
       Product.where("name ilike ?", "%#{search_term}%")
     else
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
 
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 end
